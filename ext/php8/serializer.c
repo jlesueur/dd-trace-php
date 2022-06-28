4efbd008b72b8850c5f54e0fb29fa8fcdae0faef
Bob Weinand <bob.weinand@datadoghq.com>
2022-06-28 12:48:38 +0200

    Fix
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- ext/php8/serializer.c
+++ ext/php8/serializer.c
@@ -772,30 +772,27 @@ static bool dd_rule_matches(zval *pattern, zend_string* value) {
             while (*p == '*') {
                 ++p;
             }
-            if (*p) {
-                return false;
-            }
+            free_alloca(backtrack_points, use_heap);
+            return !*p;
         }
         if (*s == *p || *p == '.') {
             ++s, ++p;
-            continue;
         } else if (*p == '*') {
             backtrack_points[backtrack_idx++] = ++p;
             backtrack_points[backtrack_idx++] = s;
-            continue;
+        } else {
+            do {
+                if (backtrack_idx > 0) {
+                    backtrack_idx -= 2;
+                    p = backtrack_points[backtrack_idx];
+                    s = ++backtrack_points[backtrack_idx + 1];
+                } else {
+                    free_alloca(backtrack_points, use_heap);
+                    return false;
+                }
+            } while (!*s);
+            backtrack_idx += 2;
         }
-
-        do {
-            if (backtrack_idx > 0) {
-                backtrack_idx -= 2;
-                p = backtrack_points[backtrack_idx];
-                s = ++backtrack_points[backtrack_idx + 1];
-            } else {
-                free_alloca(backtrack_points, use_heap);
-                return false;
-            }
-        } while (!*s);
-        backtrack_idx += 2;
     }
 
     free_alloca(backtrack_points, use_heap);
@@ -981,6 +978,8 @@ void ddtrace_serialize_span_to_array(ddtrace_span_fci *span_fci, zval *array) {
                     pthread_rwlock_unlock(&dd_span_sampling_limiter_lock);
 #endif
                 }
+
+                zend_string_release(rule_key);
 
                 if (sampling_bucket) {
                     const int nanosecond = 1000000000;
